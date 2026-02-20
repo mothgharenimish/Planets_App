9297e39c09d74d4c571ed795a456297d1c2606f9
mothgharenimish <mothgharenimish0@gmail.com>
2026-02-20 17:55:39 +0530

    Commit the Code

---
+++ Planets_App/Screens/Details/GasCircularView.swift
@@ -0,0 +1,18 @@
+//
+//  GasCircularView.swift
+//  Planets_App
+//
+//  Created by Nimish Mothghare on 19/02/26.
+//
+
+import SwiftUI
+
+struct GasCircularView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GasCircularView()
+}
