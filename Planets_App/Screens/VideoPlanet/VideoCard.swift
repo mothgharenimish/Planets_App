c54cb77060b7ac8cbdc745eed6a68c183ca54e4e
mothgharenimish <mothgharenimish0@gmail.com>
2026-02-21 01:28:42 +0530

    Commit the Video

---
+++ Planets_App/Screens/VideoPlanet/VideoCard.swift
@@ -0,0 +1,18 @@
+//
+//  VideoCard.swift
+//  Planets_App
+//
+//  Created by Nimish Mothghare on 21/02/26.
+//
+
+import SwiftUI
+
+struct VideoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoCard()
+}
