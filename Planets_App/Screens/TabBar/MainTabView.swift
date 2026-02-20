bf36b6154bd17f9192719f0cc5991527d37d1014
mothgharenimish <mothgharenimish0@gmail.com>
2026-02-21 00:55:02 +0530

    commit the code

---
+++ Planets_App/Screens/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Planets_App
+//
+//  Created by Nimish Mothghare on 20/02/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ Planets_App/Screens/VideoPlanet/VideoPlanet.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlanet.swift
+//  Planets_App
+//
+//  Created by Nimish Mothghare on 20/02/26.
+//
+
+import SwiftUI
+
+struct VideoPlanet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlanet()
+}
